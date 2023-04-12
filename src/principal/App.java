dd550607c19eff9d03e70360e51e48d623660725
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-04-12 17:24:38 -0500

    Prueba para Gato

--- src/principal/App.java
+++ src/principal/App.java
@@ -1,5 +1,6 @@
 package principal;
 
+import logica.Gato;
 import logica.Pais_Origen;
 import logica.Perro;
 import logica.Raza;
@@ -9,6 +10,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         Perro perro1 = new Perro("Kira", (byte)4, (byte)5, (byte)2, 5000000, Raza.Labradores, Pais_Origen.Colombia, Vacunas.rabia,"Dorado");
         perro1.MostrarDatos();
+        Gato gato1 = new Gato("Mia", (byte)4, (byte)5, (byte)2, 5000000, Raza.Labradores, Pais_Origen.Colombia, Vacunas.rabia,"Dorado");
+        gato1.MostrarDatos();
         
     }
 }
