47d446bc9c4db1e7244a1cf9d352834e97a36132
= <=>
2023-04-16 10:40:15 -0500

    Se hizo el cambio de los nombres de las variables para que el codigo se entienda mejor, se elimino la funcion de cargarComponentesPanel1 para evitar problemas de superposicion cada vez que se llamaba porque creaba el panel varias veces y simplemente se inicializo y se muestra y oculta cuando sea necesario, por ultimo se agrego nuevos paneles requeridos

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,9 +7,9 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class Ventana extends JFrame implements ActionListener{
-    JButton[] arregloDeBotones = new JButton[5];
-    JPanel panel0, panel1, panel2, panel3, panel4, panel5, panel6;
-    JButton boton1, boton2;
+    JButton[] botonesMenuPrincipal = new JButton[5];
+    JPanel panelPrincipal, panelMascota, panelGato, panelPerro, panel4, panel5, panel6;
+    JButton botonGato, botonPerro;
     public Ventana(){
         setSize(500, 500); 
         setTitle("Amigos Peludos");
@@ -21,42 +22,58 @@ public class Ventana extends JFrame implements ActionListener{
     }
 
     private void iniciarComponentes(){  
-        panel0 = new JPanel();    
-        this.getContentPane().add(panel0); 
-        panel0.setLayout(new GridLayout(5, 1));
+        panelPrincipal = new JPanel();    
+        this.getContentPane().add(panelPrincipal); 
+        panelPrincipal.setLayout(new GridLayout(5, 1));
             
-        for (int i = 0; i < arregloDeBotones.length; i++){
-            arregloDeBotones[i] = new JButton();
-            arregloDeBotones[i].addActionListener(this);
-            panel0.add(arregloDeBotones[i]);
+        for (int i = 0; i < botonesMenuPrincipal.length; i++){
+            botonesMenuPrincipal[i] = new JButton();
+            botonesMenuPrincipal[i].addActionListener(this);
+            panelPrincipal.add(botonesMenuPrincipal[i]);
         }
 
-        arregloDeBotones[0].setText("Insertar Mascota");
-        arregloDeBotones[1].setText("Actualizar Mascota");
-        arregloDeBotones[2].setText("Eliminar Mascota");
-        arregloDeBotones[3].setText("Buscar Mascota");
-        arregloDeBotones[4].setText("Lista de Mascotas");
-        
-        cargarComponentesPanel1();
+        botonesMenuPrincipal[0].setText("Insertar Mascota");
+        botonesMenuPrincipal[1].setText("Actualizar Mascota");
+        botonesMenuPrincipal[2].setText("Eliminar Mascota");
+        botonesMenuPrincipal[3].setText("Buscar Mascota");
+        botonesMenuPrincipal[4].setText("Lista de Mascotas");
+
+        panelMascota = new JPanel();
+        panelMascota.setLayout(new GridLayout(1, 2));
+        botonGato = new JButton("Gato");
+        botonPerro = new JButton("Perro");
+        botonGato.addActionListener(this);
+        botonPerro.addActionListener(this);
+        panelMascota.add(botonGato);
+        panelMascota.add(botonPerro);
+        panelMascota.setVisible(false);
+
+        panelGato = new JPanel();
+        panelGato.setLayout(null);
+        panelGato.setBackground(Color.CYAN);
+        panelPerro = new JPanel();
+        panelPerro.setLayout(null);
+        panelPerro.setBackground(Color.CYAN);
+
+
     }
-    private void cargarComponentesPanel1() {
-        panel1 = new JPanel();
-        panel1.setLayout(new GridLayout(1, 2));
-        boton1 = new JButton("Gato");
-        boton2 = new JButton("Perro");
-        boton1.addActionListener(this);
-        boton2.addActionListener(this);
-        panel1.add(boton1);
-        panel1.add(boton2);
-        panel1.setVisible(false);
-        }
+    
     public void actionPerformed(ActionEvent e){
-        if(e.getSource()==arregloDeBotones[0]){          
-            panel1.setVisible(true);
-            panel0.setVisible(false);
-            add(panel1);
+        if(e.getSource()==botonesMenuPrincipal[0]){          
+            panelMascota.setVisible(true);
+            panelPrincipal.setVisible(false);
+            add(panelMascota);
         }
-        
+        if(e.getSource()==botonGato){
+            panelMascota.setVisible(false);
+            panelGato.setVisible(true);
+            add(panelGato);
+        }
+        if(e.getSource()==botonPerro){
+            panelMascota.setVisible(false);
+            panelPerro.setVisible(true);
+            add(panelPerro);
+        }
     }
 }
 
