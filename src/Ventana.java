1782d0593d0a54875053ef1a7220b9a203436281
= <=>
2023-04-18 07:25:13 -0500

    Esta version del codigo debio de ser subida hoy a la 1 am pero tenia sueño, esta trae en su totalidad las opciones de insertar mascota para el botonGat, sin embargo es lo mismo para el boton perro, solo que me daba errores y me fui a dormir, ya con todas las opciones disponibles se puede enlazar con el codigo principal para que esa informacion tenga relevancia, se agregaron mas aspectos visuales como la barra deslizadora :D

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,18 +1,29 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 public class Ventana extends JFrame implements ActionListener{
-    JButton[] botonesMenuPrincipal = new JButton[5];
+    JButton[] botonesMenuPrincipal = new JButton[7];
     JPanel panelPrincipal, panelMascota, panelGato, panelPerro, panel4, panel5, panel6;
-    JButton botonGato, botonPerro;
+    JButton botonGato, botonPerro, botonEnviar;
+    JLabel etiquetaNombre, etiquetaPatas, etiquetaAños, etiquetaVacunas, etiquetaPrecio, etiquetaRaza, etiquetaPais, etiquetaColor;
+    JTextArea areaTexto;
+    int contador;
     public Ventana(){
-        setSize(500, 500); 
+        setSize(720, 480); 
         setTitle("Amigos Peludos");
+        setResizable(false);
         
         setLocationRelativeTo(null);
         iniciarComponentes();
@@ -24,11 +35,12 @@ public class Ventana extends JFrame implements ActionListener{
     private void iniciarComponentes(){  
         panelPrincipal = new JPanel();    
         this.getContentPane().add(panelPrincipal); 
-        panelPrincipal.setLayout(new GridLayout(5, 1));
+        panelPrincipal.setLayout(new GridLayout(7, 1));
             
         for (int i = 0; i < botonesMenuPrincipal.length; i++){
             botonesMenuPrincipal[i] = new JButton();
             botonesMenuPrincipal[i].addActionListener(this);
+            botonesMenuPrincipal[i].setFont(new Font("Arial",Font.BOLD,25));
             panelPrincipal.add(botonesMenuPrincipal[i]);
         }
 
@@ -37,11 +49,15 @@ public class Ventana extends JFrame implements ActionListener{
         botonesMenuPrincipal[2].setText("Eliminar Mascota");
         botonesMenuPrincipal[3].setText("Buscar Mascota");
         botonesMenuPrincipal[4].setText("Lista de Mascotas");
+        botonesMenuPrincipal[5].setText("Inyectar Vacuna");
+        botonesMenuPrincipal[6].setText("Estadisticas");
 
         panelMascota = new JPanel();
         panelMascota.setLayout(new GridLayout(1, 2));
         botonGato = new JButton("Gato");
         botonPerro = new JButton("Perro");
+        botonGato.setFont(new Font("Arial", Font.BOLD, 50));
+        botonPerro.setFont(new Font("Arial", Font.BOLD, 50));
         botonGato.addActionListener(this);
         botonPerro.addActionListener(this);
         panelMascota.add(botonGato);
@@ -49,30 +65,82 @@ public class Ventana extends JFrame implements ActionListener{
         panelMascota.setVisible(false);
 
         panelGato = new JPanel();
-        panelGato.setLayout(null);
-        panelGato.setBackground(Color.CYAN);
+        panelGato.setLayout(new GridLayout(3,1));
+        panelGato.setBackground(Color.WHITE);
         panelPerro = new JPanel();
-        panelPerro.setLayout(null);
-        panelPerro.setBackground(Color.CYAN);
+        panelPerro.setLayout(new GridLayout(3,1));
+        panelPerro.setBackground(Color.WHITE);
+        
+        etiquetaNombre = new JLabel("Ingrese el nombre de su mascota");
+        etiquetaNombre.setHorizontalAlignment(SwingConstants.CENTER);
+        etiquetaNombre.setFont(new Font("Arial", Font.BOLD,40));
+        areaTexto = new JTextArea();
+        areaTexto.setFont(new Font("Arial", Font.BOLD, 125));
+        areaTexto.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
-
+        botonEnviar = new JButton("Enviar");
+        botonEnviar.setFont(new Font("Arial",Font.BOLD,50));
+        botonEnviar.addActionListener(this);
+        panelGato.add(etiquetaNombre);
+        panelGato.add(areaTexto);
+        JScrollPane scrollPane = new JScrollPane(areaTexto, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        panelGato.add(scrollPane);
+        panelGato.add(botonEnviar); 
+        
     }
-    
     public void actionPerformed(ActionEvent e){
         if(e.getSource()==botonesMenuPrincipal[0]){          
             panelMascota.setVisible(true);
             panelPrincipal.setVisible(false);
             add(panelMascota);
         }
-        if(e.getSource()==botonGato){
+        else if(e.getSource()==botonGato){
             panelMascota.setVisible(false);
             panelGato.setVisible(true);
             add(panelGato);
         }
-        if(e.getSource()==botonPerro){
+        else if(e.getSource()==botonPerro){
             panelMascota.setVisible(false);
             panelPerro.setVisible(true);
             add(panelPerro);
+        }
+        if(e.getSource()==botonEnviar){
+            if(contador==0){
+                etiquetaNombre.setText("Ingrese la edad de su mascota");
+                areaTexto.setText("");
+                contador++;
+            }
+            else if(contador == 1){
+                etiquetaNombre.setText("Ingrese el numero de patas");
+                areaTexto.setText("");
+                contador++;
+            }
+            else if(contador == 2){
+                etiquetaNombre.setText("Ingrese el precio de su mascota");
+                areaTexto.setText("");
+                contador++;
+            }
+            else if(contador == 3){
+                etiquetaNombre.setText("Ingrese la raza de su mascota");
+                areaTexto.setText("");
+                contador++;
+            }
+            else if(contador == 4){
+                etiquetaNombre.setText("Ingrese el origen de su mascota");
+                areaTexto.setText("");
+                contador++;
+            }
+            else if(contador == 5){
+                etiquetaNombre.setText("Ingrese el color de su mascota");
+                areaTexto.setText("");
+                contador++;
+            }            
+            else{
+                contador = 0;
+                etiquetaNombre.setText("Ingrese el nombre de su mascot");
+                panelPrincipal.setVisible(true);
+                panelGato.setVisible(false);
+            }
         }
     }
 }
